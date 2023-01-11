79dc21ffa770bafade3b7376874da46b11cadda3
Max <mrheffern@gmail.com>
2023-01-11 10:56:37 -0800

    adding user accounts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "about", to: "about#index"
+
+  get "reviews", to: "reviews#index"
+  
   # Defines the root path route ("/")
   root to: "main#index"
   # root "articles#index"
